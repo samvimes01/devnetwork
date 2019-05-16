3617a761653dbc4b7bfc4eb1819e4b83331f2570
SamVimes0 <k.aleksandr88@gmail.com>
2019-05-16 23:43:27 +0300

    CRA for client setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from'./components/layout/Navbar';
+import Landing from'./components/layout/Landing';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Landing />
+    </>
+  );
+}
+
+export default App;
