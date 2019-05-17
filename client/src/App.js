034be8b139f0993cee171b0c0e952d8d851f8442
SamVimes0 <k.aleksandr88@gmail.com>
2019-05-17 08:37:26 +0300

    register login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from'./components/layout/Navbar';
 import Landing from'./components/layout/Landing';
+import Register from'./components/auth/Register';
+import Login from'./components/auth/Login';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Landing />
-    </>
+    <Router>
+      <>
+        <Navbar />
+        <section className="container">
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </>
+    </Router>
   );
 }
 
