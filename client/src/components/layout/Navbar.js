03ab9693113bc1264388e199cd5cb52e1297162d
SamVimes0 <k.aleksandr88@gmail.com>
2019-06-02 18:59:39 +0300

    comments imolemented

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,6 +9,7 @@ const Navbar = ({ logout, auth: { isAuthenticated, loading } }) => {
   const authLinks = (
     <ul>
       <li><Link to="/profiles">Developers</Link></li>
+      <li><Link to='/posts'>Posts</Link></li>
       <li>
         <Link to="/dashboard">
           <i className="fas fa-user"></i>{' '}
